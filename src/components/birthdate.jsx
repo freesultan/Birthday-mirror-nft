7522acafd1ebd079c823f9143d81d38a63d09353
Mohammad Tayyeb Soltaninejad <freetayyeb@gmail.com>
2023-09-03 23:34:00 +0330

    initial commit

---
+++ src/components/birthdate.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import Mirror from './mirror'
+import { DateTimePicker } from '@mui/x-date-pickers'
+import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
+
+const Birthinput = () => {
+  const [birthdate, setBirthdate] = useState(null);
+  const [showImage, setShowImage] = useState(false);
+  return (
+    <div className='bg-green-200 min-h-screen p-4 flex flex-col justify-center items-center '>
+      <h1 className='text-3xl'>Enter your birth time:</h1>
+
+      <div className='p-2 m-2  '>
+        <DateTimePicker
+          orientation="landscape"
+          ampm={false}
+
+          label="Select Your BirthDate"
+          value={birthdate}
+          viewRenderers={{
+            hours: renderTimeViewClock,
+            minutes: renderTimeViewClock,
+            seconds: renderTimeViewClock,
+          }}
+          views={['year', 'month', 'day', 'hours', 'minutes', 'seconds']}
+          onChange={(newValue) => { setBirthdate(newValue.toLocaleString('YYYYMMDDHH')); setShowImage(false) }} />
+      </div>
+      <button className="border drop-shadow shadow-lg border-green-600 p-2 bg-green-900 rounded-lg text-slate-300 hover:text-yellow-300"
+        onClick={() => { setShowImage(true); }}
+
+      >Generate my Birth time Image</button>
+      <h2 className='p-2 m-2 min-w-[250px]   font-bold border-green-600 drop-shadow-sm '> {birthdate == null ? '' : `You born on ${birthdate}`}</h2>
+      {showImage && birthdate &&
+        <div className=' shadow-md drop-shadow-lg border border-2 rounded border-green-600 w-[400px] h-[400px] overflow-hidden'>
+
+          <Mirror birthdate={birthdate} />
+
+        </div>
+      }
+
+    </div>
+  )
+}
+
+export default Birthinput
---
+++ src/components/mirror.jsx
@@ -0,0 +1,148 @@
+import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import { useState } from "react";
+import ColorHash from 'color-hash'
+
+
+const Mirror = ({ birthdate }) => {
+
+  var colorHash = new ColorHash();
+  const color = colorHash.hex(birthdate)
+
+  function Box(props) {
+
+    // Subscribe this component to the render-loop, rotate the mesh every frame, should be used with useRef
+    // useFrame((state, delta) => (ref.current.rotation.x += delta));
+    // Return the view, these are regular Threejs elements expressed in JSX
+    return (
+      <mesh
+        {...props}
+
+      >
+        <boxGeometry args={[0.5, 0.5, 0.2]} />
+        <meshStandardMaterial
+          metalness={1}
+          roughness={0}
+
+        />
+      </mesh>
+    );
+  }
+
+  // A cone is a convex shape by definition...
+  function Cone({ sides, ...props }) {
+
+    return (
+      <mesh castShadow  {...props}>
+        <coneGeometry args={[0.4, 0.2, sides]} />
+        <meshStandardMaterial
+          metalness={1}
+          roughness={0}
+          color={color} />
+      </mesh>
+    );
+  }
+
+  return (
+    <div className='h-[200px] min-h-screen w-full'>
+      <Canvas dpr={[1, 2]} shadows camera={{ position: [-1, -1, 3], fov: 40, near: 1, far: 20 }}>
+        {/* <spotLight
+          intensity={2}
+          position={[5, 0, 20]}
+          angle={0.2}
+          penumbra={1}
+        /> */}
+
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[-0.6, 0, 0]} />
+        <Box position={[0, 0, 0]} />
+        <Box position={[0.6, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+
+        <Box position={[-1.2, 0.6, 0]} />
+        <Box position={[-0.6, 0.6, 0]} />
+        <Box position={[0, 0.6, 0]} />
+        <Box position={[0.6, 0.6, 0]} />
+        <Box position={[1.2, 0.6, 0]} />
+
+        <Box position={[-1.2, 1.2, 0]} />
+        <Box position={[-0.6, 1.2, 0]} />
+        <Box position={[0, 1.2, 0]} />
+        <Box position={[0.6, 1.2, 0]} />
+        <Box position={[1.2, 1.2, 0]} />
+
+        <Box position={[-1.2, -0.6, 0]} />
+        <Box position={[-0.6, -0.6, 0]} />
+        <Box position={[0, -0.6, 0]} />
+        <Box position={[0.6, -0.6, 0]} />
+        <Box position={[1.2, -0.6, 0]} />
+
+        <Box position={[-1.2, -1.2, 0]} />
+        <Box position={[-0.6, -1.2, 0]} />
+        <Box position={[0, -1.2, 0]} />
+        <Box position={[0.6, -1.2, 0]} />
+        <Box position={[1.2, -1.2, 0]} />
+
+        <Cone position={[0, -1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[0, -0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[0, 0, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[0, 0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[0, 1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+
+        <Cone position={[0.6, -1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[0.6, -0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[0.6, 0, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[0.6, 0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[0.6, 1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+
+        <Cone position={[-0.6, -1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-0.6, -0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-0.6, 0, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-0.6, 0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-0.6, 1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+
+        <Cone position={[-1.2, -1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-1.2, -0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-1.2, 0, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-1.2, 0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-1.2, 1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+
+        <Cone position={[1.2, -1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[1.2, -0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[1.2, 0, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[1.2, 0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[1.2, 1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+
+        <Cone position={[1.8, -1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[1.8, -0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[1.8, 0, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[1.8, 0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[1.8, 1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+
+        <Cone position={[-1.8, -1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-1.8, -0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-1.8, 0, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-1.8, 0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-1.8, 1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+
+        <Cone position={[2.4, -1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[2.4, -0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[2.4, 0, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[2.4, 0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[2.4, 1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+
+        <Cone position={[-2.4, -1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-2.4, -0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-2.4, 0, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-2.4, 0.6, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+        <Cone position={[-2.4, 1.2, 0.2]} rotation={[1.55, 0.8, 0]} sides={4} />
+
+        <Environment preset={"sunset"} background blur={0.01} />
+        <OrbitControls enablePan={false} enableZoom={false} />
+      </Canvas>
+    </div>
+  )
+}
+
+export default Mirror
